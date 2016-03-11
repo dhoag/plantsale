3dfbd09e4de867e42ac79090c49113a12af196d6
David Hoag <davehoag@gmail.com>
2016-03-11 17:12:42 -0600

    Selection done.

--- web/app/inventory/inventory-ctrl.js
+++ web/app/inventory/inventory-ctrl.js
@@ -6,40 +6,67 @@
         var vm = this;
         vm.searchText = "";
         vm.plants = [];
-        vm.toggleOrder = toggleOrder;
         vm.toggleCat = toggleCat;
+        vm.getTotal = getTotal;
+        vm.getRunningTotal = getRunningTotal;
         vm.orders = {};
         vm.categories = [];
         initialize();
+        function getRunningTotal(){
+            var total = 0;
+            for(idx in vm.plants){
+                if(vm.plants[idx].selected)
+                    total += getTotal(vm.plants[idx]);
+            }
+            return total;
+        }
+        function getTotal(plant){
+            if(plant.limit) {
+                var total = 0;
+                for(var idx in plant.colors){
+                    total += (plant.colors[idx].qty *1);
+                }
+                return total * plant.cost;
+            }
+            return plant.qty * plant.cost;
+        }
         function toggleCat(category){
             for(var idx in vm.plants){
                 if(vm.plants[idx].type == category)
                     vm.plants[idx].category = !vm.plants[idx].category;
             }
         }
-        function toggleOrder(clicked){
-           console.log(clicked);
-        }
         function initialize()
         {
             API.$get(UrlGenerator.inventory.plants())
                 .then(function (results) {
                     vm.plants = results.data;
-                    var types = new Set();
+                    var types = {};
                     for(var idx in vm.plants){
                         vm.plants[idx].qty = 1;
-                        types.add(vm.plants[idx].type);
+                        var cat = vm.plants[idx].type;
+                        types[cat] = { name: cat, mkt: vm.plants[idx].marketing};
+
                         if(vm.plants[idx].color_preference) {
                             if(vm.plants[idx].color_limits){
                                 vm.plants[idx].limit = true;
-                                vm.plants[idx].colors = vm.plants[idx].color_limits.split(";");
+                                var colors = vm.plants[idx].color_limits.split(";");
+                                var values = [];
+                                for( var colIdx in colors){
+                                    values.push( { name: colors[colIdx], qty: 0});
+                                }
+                                vm.plants[idx].colors = values;
                             }
                             else {
-                                vm.plants[idx].free_color = "";
+                                vm.plants[idx].free_color = { name: "", qty: 0};
                             }
                         }
                     }
-                    vm.categories = Array.from(types);
+                    var result = [];
+                    for(i in types){
+                        result.push(types[i]);
+                    }
+                    vm.categories = result;
                     console.log(vm.categories);
                 })
 
