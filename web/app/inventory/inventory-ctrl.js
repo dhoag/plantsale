1b4107e45e782989d040f740fc08fbc31d3a697f
David Hoag <davehoag@gmail.com>
2016-03-11 15:04:32 -0600

    Collapsible lists.

--- web/app/inventory/inventory-ctrl.js
+++ web/app/inventory/inventory-ctrl.js
@@ -1,19 +1,48 @@
-(function(){
-   angular.module('plantsale').controller('Inventory', Inventory);
-   Inventory.$inject = ["Auth",  "API", "UrlGenerator"];
+(function () {
+    angular.module('plantsale').controller('Inventory', Inventory);
+    Inventory.$inject = ["Auth", "API", "UrlGenerator"];
 
-   function Inventory(Auth, API, UrlGenerator){
-      var vm = this;
-      vm.searchText = "";
-      vm.plants = [];
-      initialize();
+    function Inventory(Auth, API, UrlGenerator) {
+        var vm = this;
+        vm.searchText = "";
+        vm.plants = [];
+        vm.toggleOrder = toggleOrder;
+        vm.toggleCat = toggleCat;
+        vm.orders = {};
+        vm.categories = [];
+        initialize();
+        function toggleCat(category){
+            for(var idx in vm.plants){
+                if(vm.plants[idx].type == category)
+                    vm.plants[idx].category = !vm.plants[idx].category;
+            }
+        }
+        function toggleOrder(clicked){
+           console.log(clicked);
+        }
+        function initialize()
+        {
+            API.$get(UrlGenerator.inventory.plants())
+                .then(function (results) {
+                    vm.plants = results.data;
+                    var types = new Set();
+                    for(var idx in vm.plants){
+                        vm.plants[idx].qty = 1;
+                        types.add(vm.plants[idx].type);
+                        if(vm.plants[idx].color_preference) {
+                            if(vm.plants[idx].color_limits){
+                                vm.plants[idx].limit = true;
+                                vm.plants[idx].colors = vm.plants[idx].color_limits.split(";");
+                            }
+                            else {
+                                vm.plants[idx].free_color = "";
+                            }
+                        }
+                    }
+                    vm.categories = Array.from(types);
+                    console.log(vm.categories);
+                })
 
-      function initialize(){
-         API.$get(UrlGenerator.inventory.plants())
-             .then(function(results){
-                vm.plants = results.data;
-             })
-
-      }
-   }
+        }
+    }
 })();
