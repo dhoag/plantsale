a22883b54b1abeaa98d7384438f5647e7c51b40e
David Hoag <davehoag@gmail.com>
2016-03-13 12:02:11 -0500

    Initial order management.

--- web/app/components/order-svc.js
+++ web/app/components/order-svc.js
@@ -3,18 +3,22 @@
         .service("OrderSvc", OrderSvc);
     OrderSvc.$inject = [ 'API', 'UrlGenerator'];
 
-    function OrderSvc(){
+    function OrderSvc(API, UrlGenerator){
         var svc = {
             getOrders: getOrders,
             updateOrderItem: updateOrderItem,
+            deleteOrderItem: deleteOrderItem,
             addOrderItem: addOrderItem
         };
         return svc;
         function updateOrderItem(id, data){
-            return API.$patch(UrlGenerator.user.updateOrderItem(id), data);
+            return API.$patch(UrlGenerator.user.updateItem(id), data);
+        }
+        function deleteOrderItem( id){
+            return API.$delete(UrlGenerator.user.updateItem(id));
         }
         function addOrderItem( data){
-            return API.$post(UrlGenerator.user.addOrderItem(), data);
+            return API.$post(UrlGenerator.user.addItem(), data);
         }
         function getOrders(){
             return API.$get(UrlGenerator.user.orders());
--- web/app/inventory/order-builder.js
+++ web/app/inventory/order-builder.js
@@ -5,11 +5,13 @@
 
     function OrderBuilder(Auth, Inventory, OrderSvc, $scope) {
         var vm = this;
+        vm.order = null;
         vm.searchText = "";
         vm.plants = [];
         vm.toggleCat = toggleCat;
         vm.getTotal = getTotal;
         vm.getRunningTotal = getRunningTotal;
+        vm.toggleOrderItem = toggleOrderItem;
         vm.orders = {};
         vm.categories = [];
         vm.loggedIn = Auth.isLoggedIn();
@@ -38,17 +40,60 @@
                     vm.plants[idx].category = !vm.plants[idx].category;
             }
         }
+        function setOrder(){
+            OrderSvc.getOrders()
+                .then(function (ex) {
+                    vm.order = ex.data[0];
+                })
+        }
         function initialize()
         {
             Inventory.getPlants()
                 .then(function(ex){
                     vm.plants = ex;
                     vm.categories = Inventory.getCategories();
-                    $scope.$watch( vm.plants, changeHandler );
+                    if(vm.loggedIn) {
+                        setOrder();
+                    }
                 })
         }
-        function changeHandler(ex){
-            console.log(ex);
+
+        function addPlantToOrder(plant) {
+            console.log("Adding " + plant.name);
+            var data = {
+                order: vm.order.id,
+                qty: plant.qty,
+                plant: plant.id
+            };
+            OrderSvc.addOrderItem(data)
+                .then(function (ex) {
+                    plant.orderId = ex.data.id;
+                })
+                .catch(function (error) {
+                    toastr.error("Failed to add item to order", error);
+                })
+        }
+
+        function removePlantFromOrder(plant) {
+            console.log("Deleting" + plant.name);
+            OrderSvc.deleteOrderItem(plant.orderId)
+                .then(function (ex) {
+                    plant.orderId = null;
+                })
+                .catch(function (error) {
+                    toastr.error("Failed to remove item fro order", error);
+                })
+        }
+
+        function toggleOrderItem(plant){
+            if(!plant.color_preference){
+                if(plant.selected){
+                    addPlantToOrder(plant);
+                }
+                else{
+                    removePlantFromOrder(plant);
+                }
+            }
         }
     }
 })();
